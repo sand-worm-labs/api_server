d46cac4eb0119422c1017af20d5497d5558ccb18
Angry Coding <mailidanielifechukwu@gmail.com>
2025-05-26 02:18:41 +0000

    chore: black listed  string

--- src/utils.rs
+++ src/utils.rs
@@ -27,60 +27,89 @@ pub fn remove_sql_comments(sql: &str) -> String {
     no_line2.into_owned()
 }
 
-pub fn is_query_only(sql: String) -> bool {
-    const BLACKLIST: &[&str] = &[
-        "INSERT",
-        "UPDATE",
-        "DELETE",
-        "CREATE",
-        "DROP",
-        "ALTER",
-        "TRUNCATE",
-        "REPLACE",
-        "GRANT",
-        "REVOKE",
-        "SHOW",
-        "USER",
-        "SET",
-        "EXECUTE",
-        "CALL",
-        "COPY",
-        "current_database()",
-        "current_user()",
-        "session_user()",
-        "inet_client_addr()",
-        "inet_server_addr()",
-        "version()",
-        "pg_backend_pid()",
-        "pg_postmaster_start_time()",
-        "pg_current_xact_id()",
-        "pg_is_in_recovery()",
-        "txid_current()",
-        "pg_size_pretty()",
-        "USER",
-        "search_path",                         // current schema(s) being used
-        "client_encoding",                     // character encoding used by the client
-        "DateStyle",                           // date/time formatting
-        "TimeZone",                            // current timezone
-        "application_name",                    // app name used in logs/monitoring
-        "server_version",                      // PostgreSQL version
-        "is_superuser",                        // true/false if user is a superuser
-        "session_authorization",               // current session user
-        "standard_conforming_strings",         // string literal behavior
-        "transaction_isolation",               // read committed, repeatable read, etc.
-        "statement_timeout",                   // max time a query can run
-        "lock_timeout",                        // max time to wait on a lock
-        "idle_in_transaction_session_timeout", // timeout for idle tx
-        "max_connections",                     // total allowed DB connections
-        "shared_buffers",                      // memory buffer pool
-        "work_mem",                            // memory per sort/hash op
-        "maintenance_work_mem",                // memory for vacuum, create index
-        "effective_cache_size",                // planner estimate of OS cache
-        "log_min_duration_statement",          // logging slow queries
-        "log_statement",                       // what kinds of statements are logged
+ const BLACKLIST: &[&str] = &[
+        // DML
+        "INSERT", "UPDATE", "DELETE", "MERGE", "UPSERT", "TRUNCATE", "RETURNING", "OVERRIDING SYSTEM VALUE",
+
+        // DDL
+        "CREATE", "ALTER", "DROP", "RENAME", "COMMENT", "REINDEX", "CLUSTER", "DISCARD",
+
+        // Transactions
+        "BEGIN", "COMMIT", "ROLLBACK", "SAVEPOINT", "RELEASE", "PREPARE", "DEALLOCATE",
+
+        // Roles & Users
+        "GRANT", "REVOKE", "CREATE USER", "DROP USER", "CREATE ROLE", "DROP ROLE", "ALTER USER", "ALTER ROLE",
+        "SET ROLE", "RESET ROLE", "SESSION AUTHORIZATION", "SET SESSION AUTHORIZATION", "LOGIN", "PASSWORD",
+
+        // Schema & Object Management
+        "CREATE TABLE", "DROP TABLE", "ALTER TABLE", "UNLOGGED", "TEMP TABLE", "TEMPORARY",
+        "CREATE SEQUENCE", "ALTER SEQUENCE", "DROP SEQUENCE",
+        "CREATE VIEW", "DROP VIEW", "ALTER VIEW", "MATERIALIZED", "REFRESH MATERIALIZED VIEW",
+        "CREATE FUNCTION", "ALTER FUNCTION", "DROP FUNCTION",
+        "CREATE PROCEDURE", "DROP PROCEDURE", "CALL",
+        "CREATE TRIGGER", "DROP TRIGGER",
+        "CREATE RULE", "DROP RULE",
+        "CREATE INDEX", "DROP INDEX", "USING BTREE", "USING GIN", "USING HASH",
+        "CREATE EXTENSION", "ALTER EXTENSION", "DROP EXTENSION",
+        "CREATE SCHEMA", "DROP SCHEMA", "ALTER SCHEMA",
+
+        // Copy & External
+        "COPY", "DO", "LISTEN", "NOTIFY", "UNLISTEN", "EXPLAIN", "ANALYZE",
+
+        // Runtime and system information
+        "SHOW", "SET", "RESET", "CONFIG", "LOAD", "VACUUM", "ANALYZE", "CHECKPOINT", "REASSIGN OWNED",
+        "pg_sleep", "pg_cancel_backend", "pg_terminate_backend", "pg_reload_conf", "pg_rotate_logfile",
+        "pg_stat_reset", "pg_logical_emit_message",
+
+        // Low-level system functions
+        "pg_backend_pid", "pg_postmaster_start_time", "pg_current_xact_id", "txid_current",
+        "pg_is_in_recovery", "pg_last_xact_replay_timestamp", "pg_switch_wal", "pg_create_physical_replication_slot",
+        "pg_drop_replication_slot", "pg_create_logical_replication_slot", "pg_drop_logical_replication_slot",
+
+        // WAL, Replication
+        "pg_current_wal_lsn", "pg_wal_lsn_diff", "pg_replication_origin", "pg_create_restore_point",
+        "pg_start_backup", "pg_stop_backup", "pg_promote",
+
+        // System views/tables
+        "pg_stat_", "pg_replication_", "pg_settings", "pg_file_", "pg_ls_", "pg_log_", "pg_read_file",
+        "pg_read_binary_file", "pg_stat_file", "pg_tablespace", "pg_database", "pg_user", "pg_roles",
+        "pg_shadow", "pg_authid", "pg_auth_members", "pg_group",
+
+        // Size & config introspection
+        "pg_size_pretty", "pg_table_size", "pg_database_size", "pg_indexes_size",
+        "pg_total_relation_size", "pg_column_size", "pg_relation_size",
+
+        // Network/system
+        "inet_client_addr", "inet_client_port", "inet_server_addr", "inet_server_port",
+        "pg_hba_file_rules", "pg_ident_file_mappings",
+
+        // Injection & abuse patterns
+        "--", "/*", "*/", "#", ";", ";--", "OR 1=1", "' OR '1'='1", "\" OR \"1\"=\"1", "UNION SELECT",
+        "INFORMATION_SCHEMA", "SYSTEM_USER", "CURRENT_CATALOG", "CURRENT_SCHEMA",
+
+        // Encoding & internal config
+        "client_encoding", "application_name", "standard_conforming_strings",
+        "statement_timeout", "idle_in_transaction_session_timeout", "log_min_duration_statement",
+        "work_mem", "maintenance_work_mem", "shared_buffers", "effective_cache_size",
+
+        // User identity
+        "user", "current_user", "session_user", "system_user", "is_superuser",
+
+        // Time/locale
+        "datestyle", "timezone",
+
+        // Extensions or plugins
+        "plpgsql", "pgcrypto", "postgis", "pgstattuple", "snowball", "tsearch2", "uuid-ossp", "xml2","size"
     ];
-    let upper = sql.to_uppercase();
-    BLACKLIST.iter().any(|kw| upper.contains(kw))
+    
+pub fn is_query_only(sql: String) -> bool { 
+     !is_blacklisted_query(&sql)
+}
+
+fn is_blacklisted_query(sql: &str) -> bool {
+    const BLACKLIST_REGEX: &str = r###"(?i)\b(INSERT|UPDATE|DELETE|MERGE|UPSERT|TRUNCATE|RETURNING|OVERRIDING\s+SYSTEM\s+VALUE|CREATE|ALTER|DROP|RENAME|COMMENT|REINDEX|CLUSTER|DISCARD|BEGIN|COMMIT|ROLLBACK|SAVEPOINT|RELEASE|PREPARE|DEALLOCATE|GRANT|REVOKE|CREATE\s+USER|DROP\s+USER|CREATE\s+ROLE|DROP\s+ROLE|ALTER\s+USER|ALTER\s+ROLE|SET\s+ROLE|RESET\s+ROLE|SESSION\s+AUTHORIZATION|SET\s+SESSION\s+AUTHORIZATION|LOGIN|PASSWORD|CREATE\s+TABLE|DROP\s+TABLE|ALTER\s+TABLE|UNLOGGED|TEMP\s+TABLE|TEMPORARY|CREATE\s+SEQUENCE|ALTER\s+SEQUENCE|DROP\s+SEQUENCE|CREATE\s+VIEW|DROP\s+VIEW|ALTER\s+VIEW|MATERIALIZED|REFRESH\s+MATERIALIZED\s+VIEW|CREATE\s+FUNCTION|ALTER\s+FUNCTION|DROP\s+FUNCTION|CREATE\s+PROCEDURE|DROP\s+PROCEDURE|CALL|CREATE\s+TRIGGER|DROP\s+TRIGGER|CREATE\s+RULE|DROP\s+RULE|CREATE\s+INDEX|DROP\s+INDEX|USING\s+BTREE|USING\s+GIN|USING\s+HASH|CREATE\s+EXTENSION|ALTER\s+EXTENSION|DROP\s+EXTENSION|CREATE\s+SCHEMA|DROP\s+SCHEMA|ALTER\s+SCHEMA|COPY|DO|LISTEN|NOTIFY|UNLISTEN|EXPLAIN|ANALYZE|SHOW|SET|RESET|CONFIG|LOAD|VACUUM|CHECKPOINT|REASSIGN\s+OWNED|pg_sleep|pg_cancel_backend|pg_terminate_backend|pg_reload_conf|pg_rotate_logfile|pg_stat_reset|pg_logical_emit_message|pg_backend_pid|pg_postmaster_start_time|pg_current_xact_id|txid_current|pg_is_in_recovery|pg_last_xact_replay_timestamp|pg_switch_wal|pg_create_physical_replication_slot|pg_drop_replication_slot|pg_create_logical_replication_slot|pg_drop_logical_replication_slot|pg_current_wal_lsn|pg_wal_lsn_diff|pg_replication_origin|pg_create_restore_point|pg_start_backup|pg_stop_backup|pg_promote|pg_stat_|pg_replication_|pg_settings|pg_file_|pg_ls_|pg_log_|pg_read_file|pg_read_binary_file|pg_stat_file|pg_tablespace|pg_database|pg_user|pg_roles|pg_shadow|pg_authid|pg_auth_members|pg_group|pg_size_pretty|pg_table_size|pg_database_size|pg_indexes_size|pg_total_relation_size|pg_column_size|pg_relation_size|inet_client_addr|inet_client_port|inet_server_addr|inet_server_port|pg_hba_file_rules|pg_ident_file_mappings|--|/\*|\*/|#|;|;--|OR\s+1=1|' OR '1'='1|\" OR \"1\"=\"1|UNION\s+SELECT|INFORMATION_SCHEMA|SYSTEM_USER|CURRENT_CATALOG|CURRENT_SCHEMA|client_encoding|application_name|standard_conforming_strings|statement_timeout|idle_in_transaction_session_timeout|log_min_duration_statement|work_mem|maintenance_work_mem|shared_buffers|effective_cache_size|user|current_user|session_user|system_user|is_superuser|datestyle|timezone|plpgsql|pgcrypto|postgis|pgstattuple|snowball|tsearch2|uuid-ossp|xml2|size)\b"###;
+    let re = Regex::new(BLACKLIST_REGEX).unwrap();
+    re.is_match(sql)
 }
 
 pub fn is_sui_rpc_query(query: &str) -> bool {
